f3acec875381bc121cf36df9175923944761c4e9
Murad Safarzade <murad.seferzade@gmail.com>
2020-08-20 23:04:33 -0400

    Murads 1st commit

--- src/test/java/com/delta/runners/CukeRunner.java
+++ src/test/java/com/delta/runners/CukeRunner.java
@@ -11,11 +11,10 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-<<<<<<< HEAD
+
 		tags = "@temps",
-=======
-		tags = "@tempi",
->>>>>>> 44bf363fa532deec75f5b390fa8ffae733be4f0e
+
+
 		dryRun = false,
 		features = "src/test/resources/com/delta/features",
 		glue = "com/delta/stepDefs",
